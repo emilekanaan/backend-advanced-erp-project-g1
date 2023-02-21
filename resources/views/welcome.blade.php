c2835a832caf799101db1c22fba911de1a00cc36
Emile Kanaan <emilekanaan99@gmail.com>
2023-02-21 10:45:51 +0200

    Initializing laravel project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Advanced ERP Project - Group 1</title>
+    </head>
+    <body class="antialiased">
+        
+    </body>
+</html>
