c78f9b01ff347f2aba5d34339e097fcb8c6c381d
malak-wahyb <malakwahyb537@gmail.com>
2023-03-02 11:55:27 +0200

    finished the aut

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -9,90 +9,92 @@ use Illuminate\Http\Request;
 
 class EmployeeController extends Controller
 {
-    public function addEmployee(Request $request){
-        try{
-         $request->validate([
-             
-        "first_name"=> 'required',
-        "last_name"=> 'required',
-        "email"=> 'required',
-        "phone_num"=> 'required',
-        "picture"=> 'required',
-        'team_id' => 'nullable|exists:teams,id',
+    public function addEmployee(Request $request)
+    {
+        try {
+            $request->validate([
+                'first_name' => 'required',
+                'last_name' => 'required',
+                'email' => 'required',
+                'phone_num' => 'required',
+                'picture' => 'required',
+                'team_id' => 'nullable|exists:teams,id',
+            ]);
 
-         ]);
- 
-         $employee = new Employee();
-       
-       $first_name=$request->input("first_name");
-       $last_name=$request->input("last_name");
-       $email=$request->input("email");
-       $phone_num=$request->input("phone_num");
-       $picture=$request->file("picture")->store('pictures','public');
-       $team_id=$request->input('team_id');
-       $team = Team::find($team_id);
-         $employee->first_name=$first_name;
-         $employee->last_name=$last_name;
-         $employee->email=$email;
-         $employee->phone_num=$phone_num;
-         $employee->picture=$picture;
-       
+            $employee = new Employee();
 
-         $employee->team()->associate($team);
-         $employee->save();
-         return response()->json([
-             'message' => 'employee created successfully'
-         ]);}catch (\Exception $e) {
+            $first_name = $request->input('first_name');
+            $last_name = $request->input('last_name');
+            $email = $request->input('email');
+            $phone_num = $request->input('phone_num');
+            $picture = $request->file('picture')->store('pictures', 'public');
+            $team_id = $request->input('team_id');
+            $team = Team::find($team_id);
+            $employee->first_name = $first_name;
+            $employee->last_name = $last_name;
+            $employee->email = $email;
+            $employee->phone_num = $phone_num;
+            $employee->picture = $picture;
+
+            $employee->team()->associate($team);
+            $employee->save();
+            return response()->json([
+                'message' => 'employee created successfully',
+            ]);
+        } catch (\Exception $e) {
             return response()->json(['message' => 'Failed to retrieve employee.'], 500);
         }
-     }
-     public function getEmployee(Request $request, $id){
-     try{
-         $employee =  Employee::where("id",$id)->with(['team'])->get();
-     
-         return response()->json([
-             'message' => $employee,
-         ]);}catch (\Exception $e) {
-             return response()->json(['message' => 'Failed to retrieve employee.'], 500);
-         }
-     }
- 
-         public function editEmployee(Request $request, $id){
-             try{
-             $employee=Employee::find($id);
-             $inputs=$request->except('_method','team_id','picture'); 
-             if($request->has('team_id')){
-                $team_id=$request->input('team_id');
+    }
+    public function getEmployee(Request $request, $id)
+    {
+        try {
+            $employee = Employee::where('id', $id)
+                ->with(['team'])
+                ->get();
+
+            return response()->json([
+                'message' => $employee,
+            ]);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Failed to retrieve employee.'], 500);
+        }
+    }
+
+    public function editEmployee(Request $request, $id)
+    {
+        try {
+            $employee = Employee::find($id);
+            $inputs = $request->except('_method', 'team_id', 'picture');
+            if ($request->has('team_id')) {
+                $team_id = $request->input('team_id');
                 $team = Team::find($team_id);
                 $employee->team()->associate($team);
-             }
-             if($request->hasFile('picture')){
-                Storage::delete('public/'.$employee->picture);
-                $image_path = $request->file('picture')->store('images','public');
+            }
+            if ($request->hasFile('picture')) {
+                Storage::delete('public/' . $employee->picture);
+                $image_path = $request->file('picture')->store('images', 'public');
                 $employee->update(['picture' => $image_path]);
             }
-             $employee->update($inputs); 
-             return response()->json(['message' => 'employee successfully updated',
-             'employee'=>$employee
-             ])  ;
-             }catch (\Exception $e) {
-                 return response()->json(['message' => 'Failed to edit employee.'], 500);
-             }
-         }
-        
-         public function getEmployees(){
-     try {
-         $employee = Employee::with(['team'])->get();
-         return response()->json($employee, 200);
-     } catch (\Exception $e) {
-         return response()->json(['message' => 'Failed to retrieve Reports.'], 500);
-     }
- }
- public function deleteEmployee(Request $request,$id){
-   
-        $employee=Employee::find($id);
+            $employee->update($inputs);
+            return response()->json(['message' => 'employee successfully updated', 'employee' => $employee]);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Failed to edit employee.'], 500);
+        }
+    }
+
+    public function getEmployees()
+    {
+        try {
+            $employee = Employee::with(['team'])->get();
+            return response()->json($employee, 200);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Failed to retrieve Reports.'], 500);
+        }
+    }
+    public function deleteEmployee(Request $request, $id)
+    {
+        $employee = Employee::find($id);
         $employee->delete();
         return response()->json(['message' => 'employee deleted successfully']);
-    
- } 
+    }
 }
--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Middleware;
-
+use closure;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
 
@@ -10,8 +10,22 @@ class Authenticate extends Middleware
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      */
-    protected function redirectTo(Request $request): ?string
+    protected function redirectTo(Request $request)
     {
-        return $request->expectsJson() ? null : route('login');
+    
+        return $request->expectsJson() ? null : route('authenticate');
+      
+    }
+    public function handle($request, Closure $next, ...$guards)
+    {
+      
+        $jwt_name = env("JWT_SECRET");
+        if($jwt=$request->cookie($jwt_name)){
+            $request->headers->set('Authorization','Bearer ' . $jwt);
+        }  
+        $this->authenticate($request, $guards);
+
+        return $next($request);
+   
     }
 }
